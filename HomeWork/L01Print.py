4a92b88479287eea9593d9b56549ffd596811c89
TheDevConnor <connorharris427@gmail.com>
2026-01-16 14:30:57 -0500

    Finished Homework 1

---
+++ HomeWork/L01Print.py
@@ -0,0 +1,9 @@
+my_name = "Connor Harris"
+print("My name is:", my_name)
+
+print("Use 4 spaces per indentation level.")
+print("Python uses indentation to define code blocks instead of braces.")
+print("Outcomes I hope to gain from this course:")
+print("* Improve my problem-solving skills")
+print("* Write clear and efficient Python programs")
+print("* Gain a strong foundation for advanced computer science courses")
